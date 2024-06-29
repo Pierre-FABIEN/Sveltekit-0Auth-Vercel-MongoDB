482abfdcd2e9655f6123ac3c2217237d880b0e6e
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-06-29 07:19:46 +0200

    change auth and add log

--- src/lib/functions/checkAuth.ts
+++ src/lib/functions/checkAuth.ts
@@ -1,27 +1,37 @@
+// src/lib/functions/checkAuth.ts
+
 import prisma from "$lib/prisma";
 
 export const checkAuth = async (session: any) => {
-  let user
+  console.log('Session:', session);
+  let user;
 
   if (session?.user?.email) {
-
-    user = await prisma.user.findUnique({
-      where: {
-        email: session.user.email,
-      }     
-    })
-
-    if (!user) {
-      await prisma.user.create({
-        data: {
-          name: session.user.name,
+    try {
+      console.log('Fetching user with email:', session.user.email);
+      user = await prisma.user.findUnique({
+        where: {
           email: session.user.email,
-          image: session.user.image,
-          role: 'user',
         },
-      })
+      });
+      console.log('Fetched user:', user);
+
+      if (!user) {
+        console.log('Creating new user...');
+        user = await prisma.user.create({
+          data: {
+            name: session.user.name,
+            email: session.user.email,
+            image: session.user.image,
+            role: 'user',
+          },
+        });
+        console.log('Created new user:', user);
+      }
+    } catch (error) {
+      console.error('Error fetching user:', error);
     }
   }
 
-  return user
-}
+  return user;
+};
