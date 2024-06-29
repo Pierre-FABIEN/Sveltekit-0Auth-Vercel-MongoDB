ee2fdd0499770df47a7bd6d33b07c6363fcff921
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-06-29 10:35:11 +0200

    fix login

--- src/routes/protected/+page.server.ts
+++ src/routes/protected/+page.server.ts
@@ -6,10 +6,6 @@ export const load: PageServerLoad = async ({ locals }) => {
   const session = await locals.getSession();
   const user = await checkAuth(session) 
 
-  if (!session?.user) {
-    throw redirect(302, '/login');
-}
-
   if (user && session) {
     session.user.role = user.role;
   }
