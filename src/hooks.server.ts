494e96d1709b7dac4ecf6bb946680bde798b1600
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-06-29 07:10:26 +0200

    change hook.server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,15 +4,22 @@ import { handle as authHandle } from './auth';
 import { sequence } from '@sveltejs/kit/hooks';
 
 export const handle = sequence(authHandle, async ({ event, resolve }) => {
-  const session = await event.locals.getSession()
-  //console.log(session, 'oiuhesiuh');
-  
+  const session = await event.locals.getSession();
+
   if (session?.user?.email) {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: session.user.email,
-      },
-    });
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: session.user.email,
+        },
+      });
+
+      if (user) {
+        event.locals.role = user.role;
+      }
+    } catch (error) {
+      console.error('Error fetching user role:', error);
+    }
   }
 
   return resolve(event);
